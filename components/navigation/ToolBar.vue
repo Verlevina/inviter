9ddc158f31619fe6a0894698fe8cd9037fd3b9cf
Levitina Veronika <levitinava@gmail.com>
2019-05-19 22:55:27 +0300

    layout: navigation panel

---
+++ components/navigation/ToolBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon
+      class="hidden-md-and-up"
+      @click="$emit('toggleMenu')"
+    ></v-toolbar-side-icon>
+    <v-toolbar-title>
+      <h2>
+        Inviter
+      </h2>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Your events</v-btn>
+      <v-btn flat>Create event</v-btn>
+      <v-btn flat>Templates</v-btn>
+      <v-btn flat>About Project</v-btn>
+
+      <UserPreview></UserPreview>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+  import UserPreview from '~/components/user/user-preview.vue'
+
+  export default {
+    components: {
+      UserPreview
+    }
+  }
+</script>
