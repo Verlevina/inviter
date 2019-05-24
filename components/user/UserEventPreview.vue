9ece784dfcc70f85c1aaaa10a0b42fcfb9806c74
Levitina Veronika <levitinava@gmail.com>
2019-05-24 13:03:51 +0300

    user event preview style

--- components/user/UserEventPreview.vue
+++ components/user/UserEventPreview.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-card>
+  <v-card
+  class="card">
     <v-img
       src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
       aspect-ratio="2.75"
@@ -25,4 +26,19 @@
 </script>
 
 <style scoped lang="scss">
+  $hoverEasing: cubic-bezier(0.23, 1, 0.32, 1);
+  $returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  .card {
+    cursor: pointer;
+  }
+  .card:hover {
+    transition:
+      0.6s $hoverEasing,
+      box-shadow 2s $hoverEasing;
+    box-shadow:
+      rgba(white, 0.2) 0 0 40px 5px,
+      rgba(white, 1) 0 0 0 1px,
+      rgba(black, 1) 0 80px 100px 0,
+      inset white 0 0 0 6px;
+  }
 </style>
